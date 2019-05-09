d5ab408cd38e0723accfade0000d85f8495c0a7a
Chris <chris.ruettimann@gmx.net>
2019-05-09 15:16:53 +0200

    AddedGui

---
+++ src/chat/Receiver.java
@@ -0,0 +1,44 @@
+package chat;
+
+import chat.chat_vorlagen.message.Communication;
+import chat.chat_vorlagen.message.Message;
+import chat.chat_vorlagen.message.PostingMessage;
+
+import javax.swing.*;
+
+
+public class Receiver extends Thread
+{
+    private Communication communication;
+    private GUI gui;
+
+    public Receiver(Communication communication, GUI gui)
+    {
+        this.communication = communication;
+        this.gui = gui;
+    }
+
+    public void run () {
+        while (true) {
+            try {
+                communication.waitForMessage();
+
+                Message message = communication.getMessage();
+                if(message instanceof PostingMessage)
+                {
+                    Runnable appendText = new Runnable() {
+                        @Override
+                        public void run() {
+                            gui.addMessage((PostingMessage)message);
+                        }
+                    };
+                    SwingUtilities.invokeLater(appendText);
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
